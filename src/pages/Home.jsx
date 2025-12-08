3aace3198cc41e478dcf4c6167c394e7353489d9
Kunal Kumar Amar <159049370+kunalkumaramar@users.noreply.github.com>
2025-12-08 17:54:00 +0530

    deleted an unused file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import HeroSlider from "../components/Home-HeroSlider";
 import DailyDeals from "../components/Home-FeaturedProducts";
 import Brand from "../components/Home-BrandSection";
 import CollectionShowcase from "../components/Home-There'sMoreToExplore";
-import Explore from "../components/Home-ExploreSection";
+//import Explore from "../components/Home-ExploreSection";
 import PerfectFit from "../components/Home-FindYourPerfectFit";
 import ProductDetails from "../components/Home-NewProductDetail";
 import ReviewsSection from "../components/Home-ReviewsSection";
